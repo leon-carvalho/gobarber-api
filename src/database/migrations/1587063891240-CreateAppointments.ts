fcc719fbc42ff914558e824c7a55d72c54047fd4
leon-carvalho <leonardoscc15@gmail.com>
2020-04-16 17:16:44 -0300

    Fixing migration auto generate uuid error

--- src/database/migrations/1587063891240-CreateAppointments.ts
+++ src/database/migrations/1587063891240-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587063891240
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
