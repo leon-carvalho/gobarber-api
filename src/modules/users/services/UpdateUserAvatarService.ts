b99a5699db7e4c6ac6e0e87c0466e1518e9a2c26
leon-carvalho <leonardoscc15@gmail.com>
2020-05-13 17:41:54 -0300

    DDD: Fixing import errors

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,10 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import { getRepository } from 'typeorm';
 
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import User from '../models/User';
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
+import User from '../infra/typeorm/entities/User';
 
 interface Request {
   user_id: string;
