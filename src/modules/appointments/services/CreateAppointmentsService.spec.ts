98f4090520d0de88981cfc71a41d8cc68056512d
leon-carvalho <leonardoscc15@gmail.com>
2020-05-18 16:54:02 -0300

    Testing create new appointment

---
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -0,0 +1,22 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
