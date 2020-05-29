91fd5c95818dbbaa36db003f9daedf38d267aeb2
leon-carvalho <leonardoscc15@gmail.com>
2020-05-29 20:33:44 -0300

    Organizing the container

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -1,7 +1,15 @@
-import EtherealMailProvider from './implementations/EtherealMailProvider';
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+import IMailProvider from './models/IMailProvider';
 import SESMailProvider from './implementations/SESMailProvider';
+import EtherealMailProvider from './implementations/EtherealMailProvider';
 
-export default {
-  etherealMailProvider: EtherealMailProvider,
-  ses: SESMailProvider,
+const providers = {
+  ses: container.resolve(SESMailProvider),
+  ethereal: container.resolve(EtherealMailProvider),
 };
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  providers[mailConfig.driver],
+);
