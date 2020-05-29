f9565ef9ae837fd1680473903efb221c4cbab61e
leon-carvalho <leonardoscc15@gmail.com>
2020-05-28 21:59:03 -0300

    Appointments - Removing old schedules

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -12,15 +12,19 @@ describe('ListProviderDayAvailabilityService', () => {
     );
   });
 
-  it('should be able to list providers the day availables from provider', async () => {
+  it('should be able to list availability of the day from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailabity.execute({
@@ -32,22 +36,14 @@ describe('ListProviderDayAvailabilityService', () => {
 
     await expect(availability).toEqual(
       expect.arrayContaining([
-        {
-          hour: 8,
-          available: false,
-        },
-        {
-          hour: 9,
-          available: true,
-        },
-        {
-          hour: 10,
-          available: false,
-        },
-        {
-          hour: 11,
-          available: true,
-        },
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
