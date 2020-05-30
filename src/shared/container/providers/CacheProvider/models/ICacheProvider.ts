cce27bb55dc79db7c741bb4374cb92cb8f9905cf
leon-carvalho <leonardoscc15@gmail.com>
2020-05-30 14:54:40 -0300

    Invalidating cache

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   invalidate(key: string): Promise<void>;
   recover<T>(key: string): Promise<T | null>;
   save(key: string, value: any): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
