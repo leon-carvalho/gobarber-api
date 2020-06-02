4851149459d601569f8aa31cf951ca1f67860ce6
leon-carvalho <leonardoscc15@gmail.com>
2020-06-02 12:15:07 -0300

    Setting parameter name for snake-case to better serve the front end

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -23,7 +23,7 @@ passwordRouter.post(
     [Segments.BODY]: {
       token: Joi.string().uuid().required(),
       password: Joi.string().required(),
-      passwordConfirmation: Joi.string().required().valid(Joi.ref('password')),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
     },
   }),
   resetPasswordController.create,
